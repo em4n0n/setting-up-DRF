1f5c2045bc9eca3879b06bcc0fd9fee1bbe1ede4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-11 13:48:25 -0700

    Remove the api_view decorator and import HttpResponse

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
 # Create your views here.
-@api_view() # to use the response class
 def books(request):
-    return Response('list of the books',
-            status=status.HTTP_200_OK)
+    return Response('list of the books', status=status.HTTP_200_OK)
