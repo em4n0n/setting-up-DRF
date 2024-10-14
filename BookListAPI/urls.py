7f48db6b98a031c890ebd9e17b699302a504e885
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-13 17:51:06 -0700

    Create a post request

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # path('books/', views.books),
-    path('books', views.BookList.as_view()),
+    path('books/', views.BookList.as_view()),
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
-from rest_framework import APIView
+from rest_framework.views import APIView
 
 # Create your views here.
-@api_view(['GET', 'POST'])
-def books(request):
-    return Response('list of the books', status=status.HTTP_200_OK)
+# @api_view(['GET', 'POST'])
+# def books(request):
+#     return Response('list of the books', status=status.HTTP_200_OK)
 
 class BookList(APIView):
     def get(self, request):
-        Response({"message":"list of the books"}, status.HTTP_200_OK)
+        return Response({"message":"list of the books"}, status.HTTP_200_OK)
+    
+    def post(self, request):
+        return Response({"message":"new book created"}, status.HTTP_201_CREATED)
         
