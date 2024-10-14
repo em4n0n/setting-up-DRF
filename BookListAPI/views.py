4fc694689f59911e39074905babaaf57346b07a5
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-13 17:38:41 -0700

    Create a get function to return a list of the books

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
+from rest_framework import APIView
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -10,4 +11,6 @@ def books(request):
     return Response('list of the books', status=status.HTTP_200_OK)
 
 class BookList(APIView):
-    
+    def get(self, request):
+        Response({"message":"list of the books"}, status.HTTP_200_OK)
+        
