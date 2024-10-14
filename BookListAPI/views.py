814229efc18b79e0af4b0d6d2f4526d98b3c7541
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-13 19:44:50 -0700

    Create a put method

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -21,3 +21,6 @@ class BookList(APIView):
 class Book(APIView):
       def get(self, request, pk):
           return Response({"message":"single book with id" + str(pk)}, status.HTTP_200_OK)
+      
+      def put(self, request, pk):
+          return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
