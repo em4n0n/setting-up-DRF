5710a57b3178161a59dc5d80de2445d9ea9f0f00
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-13 19:48:09 -0700

    Install debug-toolbar

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -20,7 +20,7 @@ class BookList(APIView):
 
 class Book(APIView):
       def get(self, request, pk):
-          return Response({"message":"single book with id" + str(pk)}, status.HTTP_200_OK)
+          return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
       
       def put(self, request, pk):
           return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
